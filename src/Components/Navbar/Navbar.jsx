4462e9b866c740910355e529a7e56bd7b8775217
Simran Sharma <ss1043422@gmail.com>
2026-02-20 17:40:30 +0530

    Updated UI

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,13 +1,29 @@
 import React from 'react'
+import './Navbar.css'
+import { Divider } from '@mui/material';
 function Navbar() {
+
+    const handleClick = (id) => {
+        const section = document.getElementById(id);
+        if (section) {
+            section.scrollIntoView({ behavior: "smooth" });
+        }
+    };
+
     return (
         <section id='navbar'>
-            <a href='#about'>About</a>
-            <a href='#skills'>Skills</a>
-            <a href='#projects'>Projects</a>
-            <a href='#contact'>Contact Me</a>
+            <div className="navbar">
+                <a onClick={() => handleClick('about')}>About</a>
+                <a onClick={() => handleClick('skills')}>Skills</a>
+                <a onClick={() => handleClick('projects')}>Projects</a>
+                <a onClick={() => handleClick('contact')}>Contact</a>
+            </div>
+            <Divider
+                sx={{
+                    backgroundColor: "#808080"
+                }} />
         </section>
     )
 }
 
-export default Navbar
+export default Navbar;
